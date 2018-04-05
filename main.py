802d084a69e24f2290963c2f078a45baa8d25fac
Bailey Phan <bailey.phan@uky.edu>
2018-04-05 14:28:10 -0400

    Add largest clique function.

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from matplotlib import pylab
 
 DATA_FILE = 'dataset.txt'
 
+# Given a file name
+# Returns 1 if file exists, 0 otherwise
 def fileExists(filename):
 	if not os.path.exists(filename):
 		return 0
 	else:
 		return 1
 
+# Given a file name with structure of each line being [Sender, Recipient1, Recipient2, ...]
+# Returns a directed graph
 def createGraphFromFile(filename):
 
 	G = nx.DiGraph()
@@ -30,12 +34,10 @@ def createGraphFromFile(filename):
 			recipients = tokens[1:-1] # -1 ignores the \n at end of each line
 
 
-			#print('sender:', sender, 'recipients:', recipients)
-
 			# Create a directed edge from sender to each recipient
 			if sender not in G:
-				G.add_node(sender)
 				#print("Adding sender:", sender)
+				G.add_node(sender)
 
 			for recipient in recipients:
 
@@ -48,6 +50,42 @@ def createGraphFromFile(filename):
 
 	return G
 
+# Given the graph and a list of cliques (use sets because order does not matter in a clique)
+# Returns a list of the largest cliques	
+def largestClique(G, cliques):
+
+	#print('Clique size:', len(cliques[0]), 'Number of cliques:', len(cliques))
+
+	newCliques = [] # Will hold cliques of size + 1
+
+	# See if any of the nodes in a clique has a neighbor that can be added to increase clique size by 1
+	for clique in cliques: # For each clique
+		for clique_node in clique: # For each node in the clique
+			for neighbor in G.successors(clique_node): # For each neighbor of the node
+				# Avoid redundancy
+				if neighbor in clique:
+					continue
+				# Check if this neighbor can be added to the clique
+				addToClique = True
+				# If all nodes in the clique have an edges to/from this neighbor
+				for node in clique:
+					if not ( G.has_edge(node, neighbor) and G.has_edge(neighbor,node) ):
+						addToClique = False
+						break
+				if addToClique:
+					# Make sure not to add duplicates
+					newSet = clique.union([neighbor])
+					if newSet not in newCliques:
+						newCliques.append(newSet)
+
+	# Base case: cannot increase size of any clique
+	if len(newCliques) < 1:
+		return cliques
+
+	# Run again with our cliques of size + 1
+	return largestClique(G, newCliques)
+
+
 
 def main():
 
@@ -56,9 +94,27 @@ def main():
 		print('Error:', DATA_FILE, 'cannot be read. Make sure it is a valid file and try again.')
 		return 0
 
+	print("Creating graph...")
 	G = createGraphFromFile(DATA_FILE)
 
-	nx.write_gexf(G, "test.gexf")
+	
+
+	# Create a list of the nodes
+	allNodes = []
+	for node in G.nodes:
+		if len(node) < 1 or node == '\n':
+			continue
+		allNodes.append(set([node])) # Cast to list before set otherwise will be a character-wise set, rather than a string-wise set
+
+	# Find the largest clique
+	print("Determining largest clique size...")
+	largest = largestClique(G, allNodes)
+	print('The largest clique size is',len(largest[0]),'with',len(largest),'cliques.')
+
+
+	#print(G.number_of_nodes(), G.number_of_edges())
+
+	#nx.write_gexf(G, "test.gexf") #Write to file to be read into Grephi for visualization
 
 
 
